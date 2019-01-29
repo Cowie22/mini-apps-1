64b3f117907fddc897664e302ac21c8769e6ada5
Ryan Cowie <ryancowie@MacBook-Pro.local>
2019-01-28 19:59:48 -0800

    Refreshes table on reset and does not allow box to be clicked after one click

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,2 +1,13 @@
+document.addEventListener('DOMContentLoaded', function() {
 
+  document.addEventListener('click', (event) => {
+    if (event.target.matches('.btn')) {
+      window.location.reload();
+    }
+    if (event.target.matches('.square') && event.target.value === undefined) {
+      event.target.value = 'X'
+      event.target.append('X')
+    }
+  })
+});
 
