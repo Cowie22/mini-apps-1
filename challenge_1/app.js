c70cabf9b013d2579c5dd8eb19bd834d98b65d3a
Ryan Cowie <ryancowie@MacBook-Pro.local>
2019-01-28 20:06:12 -0800

    Switches values between x and y

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,12 +1,19 @@
 document.addEventListener('DOMContentLoaded', function() {
 
+  let xTurn = true;
+
   document.addEventListener('click', (event) => {
     if (event.target.matches('.btn')) {
       window.location.reload();
     }
-    if (event.target.matches('.square') && event.target.value === undefined) {
+    if (event.target.matches('.square') && event.target.value === undefined && xTurn) {
       event.target.value = 'X'
       event.target.append('X')
+      xTurn = false;
+    } else if (event.target.matches('.square') && event.target.value === undefined && !xTurn) {
+      event.target.value = 'Y';
+      event.target.append('Y');
+      xTurn = true;
     }
   })
 });
