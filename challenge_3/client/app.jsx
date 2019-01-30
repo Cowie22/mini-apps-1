1965a02a29c2ca1bb254a0b3654fa9d0f4c8333a
Ryan Cowie <ryancowie@MacBook-Pro.local>
2019-01-30 14:45:09 -0800

    Completed initial set-up with react and server

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,2 +1,31 @@
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: '',
+      mail: '',
+      password: ''
+    };
+  }
+  render() {
+    return (
+      <div>
+        <form>
+          <label>
+            <input type="text" value={this.state.name} />
+          </label>
+          <label>
+            <input type="text" value={this.state.name} />
+          </label>
+          <label>
+            <input type="text" value={this.state.name} />
+          </label>
+        </form>
+      </div>
+    )
+  }
+}
 
+ReactDOM.render(<App />, document.getElementById('app'));
+//export default App
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,2 +1,12 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 6969;
+
+app.use(express.static(path.join(__dirname, '/public')));
 
 
+app.listen(PORT, () => {
+  console.log(`NOW LISTENING ON PORT ${PORT}`)
+});
