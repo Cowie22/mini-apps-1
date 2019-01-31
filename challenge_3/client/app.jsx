6e5c0cc4ff3cbbf73ed528adbfcdf677c15e72e4
Ryan Cowie <ryancowie@MacBook-Pro.local>
2019-01-30 19:04:49 -0800

    Forms made, need to work out bugs on going to next page

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -11,6 +11,7 @@ class App extends React.Component {
     };
     this.handleCheckoutClick = this.handleCheckoutClick.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
+    this.handleAccountSubmit = this.handleAccountSubmit.bind(this);
   }
   handleCheckoutClick() {
     this.setState({checkout: !this.state.checkout});
@@ -28,20 +29,33 @@ class App extends React.Component {
     const account = this.state.accountStatus;
     return (
       <div>
-        Shopping Cart
         {!checked ? (
-          <input type="submit" value="CHECKOUT" onClick={this.handleCheckoutClick}/>
+        <HomePage onClick={this.handleCheckoutClick} />
         ):
-        <Form1 onChange={this.handleInputChange} />
-      }
+        checked ? (
+        <Form1 onChange={this.handleInputChange} onClick={this.handleAccountSubmit} />
+        ):
+        account ? (
+        <Form2 />
+        ):
+        account ? (
+        <Form3 />
+        ):
+        <Confirmation />
+        }
       </div>
     )
   }
 }
 
-let Info;
+var HomePage = (props) => (
+  <div>
+    <h3>Home Page</h3>
+    <input type="submit" value="CHECKOUT" onClick={props.onClick} />
+  </div>
+)
+
 var Form1 = (props) => (
-  info =
   <form>
     <h3>Create Account Please</h3>
     <label>
@@ -56,15 +70,73 @@ var Form1 = (props) => (
       Password:
       <input type="text" name="password" placeholder="Insert Password" onChange={props.onChange} />
     </label>
-    <input type="submit" value="To Shipping" />
+    <input type="submit" value="To Shipping" onClick={props.onClick} />
   </form>
+)
+
+var Form2 = (props) => (
+  <form>
+    <h3>Shipping Info</h3>
+    <label>
+      Address:
+      <input type="text" name="line1" placeholder="Line 1" />
+      <input type="text" name="line2" placeholder="Line 2" />
+    </label>
+    <label>
+      City:
+      <input type="text" name="city" placeholder="Insert City" />
+    </label>
+    <label>
+      State:
+      <input type="text" name="state" placeholder="Insert State" />
+    </label>
+    <label>
+      Zipcode:
+      <input type="text" name="zipcode" placeholder="Insert Zipcode" />
+    </label>
+    <label>
+      Phone#:
+      <input type="text" name="Phone#" placeholder="Insert Phone#" />
+    </label>
+    <input type="submit" value="To Billing" />
+  </form>
+)
+
+var Form3 = (props) => (
+  <form>
+    <h3>Billing Info</h3>
+    <label>
+      Credit Card:
+      <input type="text" name="Card" placeholder="Insert Card" />
+    </label>
+    <label>
+      Exp Date:
+      <input type="text" name="Exp Date" placeholder="Insert Exp Date" />
+    </label>
+    <label>
+      Cvv:
+      <input type="text" name="Cvv" placeholder="Insert Cvv" />
+    </label>
+    <label>
+      Billing Zip Code:
+      <input type="text" name="Billing" placeholder="Insert Billing Zip Code" />
+    </label>
+    <input type="submit" value="To Confirmation" />
+  </form>
+)
+
+var Confirmation = (props) => (
+  <div>
+    <h3>Confirmation</h3>
+    <input type="submit" value="PURCHASE" />
+  </div>
 )
 
 ReactDOM.render(<App />, document.getElementById('app'));
 //export default App
 
 // var Form1 = function() {
-//      return( 
+//      return(
 //      <div>
 //         Create Account Please
 //         <form>
