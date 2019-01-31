ffd94ed2008d1b562c55b40efb17460c5cbfc6b4
Ryan Cowie <ryancowie@MacBook-Pro.local>
2019-01-30 17:01:44 -0800

    Refactored the first form for more readability

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -6,13 +6,17 @@ class App extends React.Component {
       checkout: false,
       name: '',
       mail: '',
-      password: ''
+      password: '',
+      accountStatus: false
     };
     this.handleCheckoutClick = this.handleCheckoutClick.bind(this);
     this.handleInputChange = this.handleInputChange.bind(this);
   }
   handleCheckoutClick() {
     this.setState({checkout: !this.state.checkout});
+  }
+  handleAccountSubmit() {
+    this.setState({accountStatus: !this.state.accountStatus})
   }
   handleInputChange(event) {
     const newState = {};
@@ -21,35 +25,40 @@ class App extends React.Component {
   }
   render() {
     const checked = this.state.checkout;
+    const account = this.state.accountStatus;
     return (
       <div>
         Shopping Cart
         {!checked ? (
           <input type="submit" value="CHECKOUT" onClick={this.handleCheckoutClick}/>
         ):
-        <div>
-        Create Account Please
-        <form>
-          <label>
-            <input type="text" name="name" placeholder="Insert Name"
-            value={this.state.name} onChange={this.handleInputChange}/>
-          </label>
-          <label>
-            <input type="text" name="mail" placeholder="Insert Email"
-            value={this.state.mail} onChange={this.handleInputChange}/>
-          </label>
-          <label>
-            <input type="text" name="password" placeholder="Insert Password"
-            value={this.state.password} onChange={this.handleInputChange}/>
-          </label>
-          <input type="submit" value="NEXT" />
-        </form>
-      </div>
+        <Form1 onChange={this.handleInputChange} />
       }
       </div>
     )
   }
 }
+
+let Info;
+var Form1 = (props) => (
+  info =
+  <form>
+    <h3>Create Account Please</h3>
+    <label>
+      Name:
+      <input type="text" name="name" placeholder="Insert Name" onChange={props.onChange} />
+    </label>
+    <label>
+      Email:
+      <input type="text" name="mail" placeholder="Insert Email" onChange={props.onChange} />
+    </label>
+    <label>
+      Password:
+      <input type="text" name="password" placeholder="Insert Password" onChange={props.onChange} />
+    </label>
+    <input type="submit" value="To Shipping" />
+  </form>
+)
 
 ReactDOM.render(<App />, document.getElementById('app'));
 //export default App
