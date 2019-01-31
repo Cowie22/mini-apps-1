0fe0da8c098517d741734f4dbdabad74f1ef155f
Ryan Cowie <ryancowie@MacBook-Pro.local>
2019-01-30 23:58:15 -0800

    Attempt to set-up database and server connection

--- challenge_3/db.js
+++ challenge_3/db.js
@@ -8,4 +8,17 @@ const connection = mysql.createConnection({
 
 connection.connect();
 
-module.exports = connection;
+const addUser = (user, callback) => {
+  var query = "INSERT INTO accounts (name, email, password, line1, line2, city, state, zipcode, phone, Card, ExpDate, BillZip) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+  var params = [user.name, user.email, user.password, user.line1, user.line2, user.city, user.state, user.zipcode, user.phone, user.Card, user.ExpDate, user.BillZip];
+  connection.run(query, params, (err) => {
+    if (err) {
+      callback(err);
+      return;
+    }
+    callback(null);
+  })
+}
+
+module.exports.connection = connection;
+module.exports.addUser = addUser;
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const db = require('./db.js');
 
 const app = express();
 const PORT = 6969;
 
+
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(bodyParser.json());
+
+app.post('/user', (req, res,) => {
+  db.addUser(req.body, (err) => {
+    if (err) {
+      res.status(400).send()
+      return;
+    }
+    res.status(200).send();
+  })
+})
 
 
 app.listen(PORT, () => {
